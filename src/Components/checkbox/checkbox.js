206ec406d9693ce80f33c4e46172f69c76b6ffca
Aftab Baig <aftab.a.baig@gmail.com>
2018-12-23 15:55:21 -0600

    design changes

--- src/Components/checkbox/checkbox.js
+++ src/Components/checkbox/checkbox.js
@@ -7,8 +7,9 @@ export default class Checkbox extends Component {
       checked: false,
       disabled: true,
       room2: true,
-      room3: true, 
-      room4: true 
+      room3: true,
+      room4: true
+      
     };
     this.handleCheck = this.handleCheck.bind(this);
     this.handleDisable = this.handleDisable.bind(this);
@@ -20,47 +21,45 @@ export default class Checkbox extends Component {
   handleCheck() {
     //setting the logic for the disabled state of each room depending on if the checkbox is checked
     this.setState({ checked: !this.state.checked });
-    if ( document.getElementById("2").checked === true
-    ) {
-      this.setState({room2: false })
-    } 
-    if (document.getElementById("2").checked === false)
-    {
-      this.setState({room2: true })
+    if (document.getElementById("2").checked === true) {
+      this.setState({ room2: false });
     }
-    if ( document.getElementById("3").checked === true
-  ) {
-    this.setState({room2: false })
-  } 
-  if (document.getElementById("3").checked === false)
-  {
-    this.setState({room2: true })
-  }
-  if ( document.getElementById("2").checked === true
-    ) {
-      this.setState({room2: false })
-    } 
-    if (document.getElementById("4").checked === false)
-    {
-      this.setState({room2: true })
+    if (document.getElementById("2").checked === false) {
+      this.setState({ room2: true });
     }
-    
-    console.log(`room2:${this.state.room2} , room3:${this.state.room3}, room4:${this.state.room4}` );
+    if (document.getElementById("3").checked === true) {
+      this.setState({ room2: false });
+    }
+    if (document.getElementById("3").checked === false) {
+      this.setState({ room2: true });
+    }
+    if (document.getElementById("2").checked === true) {
+      this.setState({ room2: false });
+    }
+    if (document.getElementById("4").checked === false) {
+      this.setState({ room2: true });
+    }
+
+    console.log(
+      `room2:${this.state.room2} , room3:${this.state.room3}, room4:${
+        this.state.room4
+      }`
+    );
   }
   handleDisable() {
     // console.log(
     //   document.getElementById(this.props.number).checked,
     //   this.props.number
     // );
-      //if checkbox 3 is checked then 2 must be checked as well
-    this.setState({ checked: !this.state.checked });
+    //if checkbox 3 is checked then 2 must be checked as well
+    // this.setState({ checked: !this.state.checked });
     if (
       this.props.number === "3" &&
       document.getElementById("3").checked === true
     ) {
       document.getElementById("2").checked = true;
-    } 
-    //if checkbox 4 is checked, then checboxes 2 & 3 must also be checked 
+    }
+    //if checkbox 4 is checked, then checboxes 2 & 3 must also be checked
     else if (
       this.props.number === "4" &&
       document.getElementById("4").checked === true
@@ -68,7 +67,7 @@ export default class Checkbox extends Component {
       document.getElementById("2").checked = true;
       document.getElementById("3").checked = true;
     }
-    //if checkbox 2 is off, then checkbox 3 & 4 must be off 
+    //if checkbox 2 is off, then checkbox 3 & 4 must be off
     if (
       // this.props.number === "3" &&
       document.getElementById("2").checked === false
@@ -76,17 +75,16 @@ export default class Checkbox extends Component {
       document.getElementById("3").checked = false;
       document.getElementById("4").checked = false;
     }
-    //if checkbox 3 or 2 is off then checkbox 4 must be off 
-   if (
+    //if checkbox 3 or 2 is off then checkbox 4 must be off
+    if (
       // this.props.number === "3" &&
-      document.getElementById("3").checked === false 
+      document.getElementById("3").checked === false
       // document.getElementById("2").checked === false
     ) {
       document.getElementById("4").checked = false;
     }
 
-
-//if the room2 checkbox is on, then the adults & children dropdown will be enabled
+    //if the room2 checkbox is on, then the adults & children dropdown will be enabled
     if (document.getElementById("2").checked === true) {
       document.getElementById("two").disabled = false;
       document.getElementById("two-child").disabled = false;
@@ -94,7 +92,7 @@ export default class Checkbox extends Component {
       document.getElementById("two").disabled = true;
       document.getElementById("two-child").disabled = true;
     }
-//if the room 3 checkbox is on, then the adult and children drop down will be enabled and vice versa 
+    //if the room 3 checkbox is on, then the adult and children drop down will be enabled and vice versa
     if (document.getElementById("3").checked === true) {
       document.getElementById("three").disabled = false;
       document.getElementById("three-child").disabled = false;
@@ -102,7 +100,7 @@ export default class Checkbox extends Component {
       document.getElementById("three").disabled = true;
       document.getElementById("three-child").disabled = true;
     }
-//if the room 4 checkbox is on, then the adult and children drop down will be enabled and vice versa 
+    //if the room 4 checkbox is on, then the adult and children drop down will be enabled and vice versa
 
     if (document.getElementById("4").checked === true) {
       document.getElementById("four").disabled = false;
@@ -172,3 +170,5 @@ export default class Checkbox extends Component {
     );
   }
 }
+//put each room in an array
+//
